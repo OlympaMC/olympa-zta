736e06de19e7ec03daf95ce440cc0c0833c987c8
Tristiisch <tristiisch74@hotmail.com>
2021-08-15 09:10:43 +0200

    disable sitting feature

--- src/java/fr/olympa/zta/OlympaZTA.java
+++ src/java/fr/olympa/zta/OlympaZTA.java
@@ -276,12 +276,12 @@ public class OlympaZTA extends OlympaAPIPlugin implements Listener {
 				}
 			}, this);
 			pluginManager.registerEvents(crates = new CratesManager(), this);
-			pluginManager.registerEvents(new SitManager(this) {
+			/*pluginManager.registerEvents(new SitManager(this) {
 				@Override
 				public boolean canSit(Player p) {
 					return super.canSit(p) && !combat.isInCombat(p);
 				}
-			}, this);
+			}, this);*/ 
 			if (beautyQuestsLink != null)
 				pluginManager.registerEvents(beautyQuestsLink, this);
 
